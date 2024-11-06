6291b7d409ad62761f585bb5ab82410d3bd0126e
Eduardo <EduardoVG@proton.me>
2024-11-05 18:42:44 -0600

    Add @tailwindcss/typography plugin and highlight color to Tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        highlight: "#12F7D6",
+      },
       fontFamily: {
         ubuntu: ["Ubuntu", "sans-serif"],
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
